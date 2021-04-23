45313bc9af0ec1e8a8bcdf7d9901b7117eee765e
alex8224 <alex8224@gmail.com>
2021-04-23 21:08:58 +0800

    update testcase

--- src/micro.rs
+++ src/micro.rs
@@ -147,18 +147,28 @@ struct Container<T> {
     value: T,
 }
 
-impl <T>Container<T> {
+impl<T> Container<T> {
     pub fn new(value: T) -> Self {
-    Container { value }
+        Container { value }
+    }
+
+    fn println(&self) -> &T {
+        &self.value
     }
 }
 
 #[test]
 fn test_generic() {
+    let container = Container::new(r#"1111"#);
+    let val = container.println();
+    println!("{}", val);
     assert_eq!(Container::new(42).value, 42);
     assert_eq!(Container::new(3.14).value, 3.14);
     assert_eq!(Container::new("Foo").value, "Foo");
-    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(
+        Container::new(String::from("Bar")).value,
+        String::from("Bar")
+    );
     assert_eq!(Container::new(true).value, true);
     assert_eq!(Container::new(-12).value, -12);
     assert_eq!(Container::new(Some("text")).value, Some("text"));
