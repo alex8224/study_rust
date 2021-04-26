2005b50d1aa85fb7a0dea11a3b467a84b5f9b360
alex8224 <alex8224@gmail.com>
2021-04-26 22:36:07 +0800

    add testcases

--- src/micro.rs
+++ src/micro.rs
@@ -262,3 +262,22 @@ fn test_iter_generic() {
         ]
     )
 }
+
+pub fn is_even(num: i32) -> bool {
+    num % 2 == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_true_when_even() {
+        assert!(is_even(2), true);
+    }
+
+    #[test]
+    fn is_false_when_odd() {
+        assert!(!is_even(3), true);
+    }
+}
