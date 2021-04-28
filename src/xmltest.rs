73b61e67954dc9073d35753bc1b0f91c9fd1fec8
alex8224 <alex8224@gmail.com>
2021-04-29 00:25:16 +0800

    update xmltestcase

--- src/xmltest.rs
+++ src/xmltest.rs
@@ -1,6 +1,10 @@
 extern crate quick_xml;
-use quick_xml::events::Event;
+extern crate serde;
+use quick_xml::events::{BytesEnd, BytesStart, Event};
 use quick_xml::Reader;
+use quick_xml::Writer;
+use std::io::Cursor;
+use std::iter;
 
 #[test]
 fn test_basic_xml() {
@@ -41,3 +45,47 @@ fn test_basic_xml() {
         buf.clear();
     }
 }
+
+#[test]
+fn test_xml_write() {
+    let xml = r#"<this_tag k1="v1" k2="v2"><child>text</child></this_tag>"#;
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+    let mut writer = Writer::new(Cursor::new(Vec::new()));
+    let mut buf = Vec::new();
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) if e.name() == b"this_tag" => {
+                // crates a new element ... alternatively we could reuse `e` by calling
+                // `e.into_owned()`
+                let mut elem = BytesStart::owned(b"my_elem".to_vec(), "my_elem".len());
+
+                // collect existing attributes
+                elem.extend_attributes(e.attributes().map(|attr| attr.unwrap()));
+
+                // copy existing attributes, adds a new my-key="some value" attribute
+                elem.push_attribute(("my-key", "some value"));
+
+                // writes the event to the writer
+                assert!(writer.write_event(Event::Start(elem)).is_ok());
+            }
+            Ok(Event::End(ref e)) if e.name() == b"this_tag" => {
+                assert!(writer
+                    .write_event(Event::End(BytesEnd::borrowed(b"my_elem")))
+                    .is_ok());
+            }
+            Ok(Event::Eof) => break,
+            // you can use either `e` or `&e` if you don't want to move the event
+            Ok(e) => assert!(writer.write_event(&e).is_ok()),
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+        }
+        buf.clear();
+    }
+
+    let result = writer.into_inner().into_inner();
+    let expected = r#"<my_elem k1="v1" k2="v2" my-key="some value"><child>text</child></my_elem>"#;
+    assert_eq!(result, expected.as_bytes());
+}
+
+use quick_xml::de::{from_str, DeError};
+use serde::Deserialize;
